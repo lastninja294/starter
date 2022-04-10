082ff9817a831d2bc729a683723a409081281997
NorimboyevErkin <norimboyev19@gmail.com>
2022-04-10 06:28:32 -0700

    add video and image

--- src/pages/dashboard/Events/components/events-image/EventsImage.js
+++ src/pages/dashboard/Events/components/events-image/EventsImage.js
@@ -1,11 +1,26 @@
-import {Button} from 'antd';
+import React, {useState} from 'react';
+import {Button, Image} from 'antd';
 import {IoImageOutline} from 'react-icons/io5';
 
 function EventsImage() {
+  const [visible, setVisible] = useState(false);
   return (
-    <Button type='link'>
-      <IoImageOutline style={{fontSize: '1.3em'}} />
-    </Button>
+    <>
+      <Button type='link'>
+        <IoImageOutline
+          style={{fontSize: '1.3em'}}
+          onClick={() => setVisible(true)}
+        />
+      </Button>
+      <div style={{display: 'none'}}>
+        <Image.PreviewGroup
+          preview={{visible, onVisibleChange: (vis) => setVisible(vis)}}>
+          <Image src='https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp' />
+          <Image src='https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp' />
+          <Image src='https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp' />
+        </Image.PreviewGroup>
+      </div>
+    </>
   );
 }
 
--- src/pages/dashboard/Events/components/events-video/EventsVideo.js
+++ src/pages/dashboard/Events/components/events-video/EventsVideo.js
@@ -1,11 +1,36 @@
-import {Button} from 'antd';
+import {Button, Modal} from 'antd';
+import React, {useState} from 'react';
 import {AiOutlineVideoCamera} from 'react-icons/ai';
+// import ReactPlayer from 'react-player';
+import './EventsVideo.styles.scss';
 
 function EventsVideo() {
+  const [visible, setVisible] = useState(false);
   return (
-    <Button type='link'>
-      <AiOutlineVideoCamera style={{fontSize: '1.3em'}} />
-    </Button>
+    <>
+      <Button type='link' onClick={() => setVisible(true)}>
+        <AiOutlineVideoCamera style={{fontSize: '1.3em'}} />
+      </Button>
+      <Modal
+        title='Video'
+        centered
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        style={{minWidth: '70%'}}
+        footer={null}>
+        <video controls className='event-modal-video'>
+          <source
+            src='https://www.w3schools.com/html/mov_bbb.mp4'
+            type='video/mp4'
+          />
+          <source
+            src='https://www.w3schools.com/html/mov_bbb.ogg'
+            type='video/ogg'
+          />
+        </video>
+      </Modal>
+    </>
   );
 }
 
