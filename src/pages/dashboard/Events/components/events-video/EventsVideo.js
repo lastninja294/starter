6bd4119abb5acf8a815c375a70fa86818f8ad96b
NorimboyevErkin <norimboyev19@gmail.com>
2022-04-11 21:37:51 -0700

    text ellisis , carousel video modal

--- src/pages/dashboard/Events/components/events-video/EventsVideo.js
+++ src/pages/dashboard/Events/components/events-video/EventsVideo.js
@@ -4,6 +4,18 @@ import {AiOutlineVideoCamera} from 'react-icons/ai';
 // import ReactPlayer from 'react-player';
 import './EventsVideo.styles.scss';
 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
 function EventsVideo({item}) {
   const [visible, setVisible] = useState(false);
   return (
@@ -11,29 +23,29 @@ function EventsVideo({item}) {
       <Button type='link' onClick={() => setVisible(true)}>
         <AiOutlineVideoCamera style={{fontSize: '1.3em'}} />
       </Button>
-      {/* <style jsx>
+      <style jsx='true'>
         {`
           .ant-modal-body {
             padding: 0 !important;
             line-height: 0 !important;
           }
         `}
-      </style> */}
+      </style>
       <Modal
         title={item.title}
         centered
         visible={visible}
         onOk={() => setVisible(false)}
         onCancel={() => setVisible(false)}
-        style={{minWidth: '70%', maxWidth: '100%'}}
+        style={{minWidth: '70%', maxWidth: '100%', position: 'relative'}}
         footer={null}>
-        <video controls className='event-modal-video'>
-          <source src={item.videoUrl} type='video/mp4' />
-          <source
-            src='https://www.w3schools.com/html/mov_bbb.ogg'
-            type='video/ogg'
-          />
-        </video>
+        <Slider className='event-video-slide' {...settings}>
+          {item.videoUrl.map((url) => (
+            <video controls className='event-modal-video' key={url}>
+              <source src={url} type='video/mp4' />
+            </video>
+          ))}
+        </Slider>
       </Modal>
     </>
   );
